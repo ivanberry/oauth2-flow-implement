12fe26538e12105fc9e1b827748972206caf3eca
xx_vim <tabvim.dev@gmail.com>
2019-04-12 18:25:54 +0800

    update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -11,16 +12,40 @@ type Response struct {
 	http.ResponseWriter
 }
 
+// Message custom message
+type Message struct {
+	appID string
+	scope string
+	state string
+}
+
+func (r *Response) text(code int, body string) {
+	r.Header().Set("Content-Type", "text/plain")
+	r.WriteHeader(code)
+	io.WriteString(r, fmt.Sprintf("%s\n", body))
+}
+
 func main() {
 
 	// get token endpoint
-	http.HandleFunc("/oauth2/token", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/oauth2/authorize", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println(r.FormValue("app_id"))
+		resp := Response{w}
+
+		// populate msg
+		msg := &Message{
+			appID: r.FormValue("app_id"),
+			scope: r.FormValue("scope"),
+			state: r.FormValue("state"),
+		}
+
+		fmt.Println(msg)
 
 		if r.Method != "POST" {
-			fmt.Fprint(w, "Error Method verber")
+			resp.text(http.StatusNotFound, "Not Found")
 		} else {
-			resp := Response{w}
-			resp.Text(http.StatusOK, fmt.Fprint("Hello, %s", "get token"))
+			resp.text(http.StatusOK, "Hello Token")
 		}
 
 		// check all paramters
@@ -29,6 +54,7 @@ func main() {
 		// redirect_url
 		// scope
 		// state[CSRF]
+
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
